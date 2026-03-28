48e1db3d6e9d319e073c7ed2941a55596734c056
amnexya <jack@eilles.xyz>
2026-03-28 22:42:40 +0000

    update information

--- app/__init__.py
+++ app/__init__.py
@@ -36,31 +36,9 @@ for c in config_location:
 if config is None:
     exit(f"{Colours.red}[ERROR] {Colours.endc} No config file found.")
 
-print(f"{Colours.green}[INFO] {Colours.endc}Starting S3 client...")
-# Open s3 storage
-try:
-    s3 = boto3.resource('s3',
-    endpoint_url=config['endpoint'],
-    aws_access_key_id=config['access'],
-    aws_secret_access_key=config['secret'],
-    aws_session_token=None,
-    config=boto3.session.Config(signature_version='s3v4'),
-    verify=True
-    )
-
-    s3_client = boto3.client('s3',
-        endpoint_url=config['endpoint'],
-        aws_access_key_id=config['access'],
-        aws_secret_access_key=config['secret'],
-        aws_session_token=None,
-        config=boto3.session.Config(signature_version='s3v4'),
-        verify=True
-        )
-except Exception as e:
-    print(f"{Colours.red}[ERROR] {Colours.endc}S3 client could not be started, see error below.")
-    exit(e)
-
-print(f"{Colours.green}[INFO] {Colours.endc}S3 Client started.")
+print(f"{Colours.green}[INFO] {Colours.endc}Checking for local file directory...")
+if not os.path.exists(config['local_data']):
+    exit(f"{Colours.red}[ERROR] {Colours.endc}Local data directory not found.")
 
 print(f"{Colours.green}[INFO] {Colours.endc}Starting WSGI...")
 
@@ -69,9 +47,8 @@ app = Flask(__name__)
 # Database URI
 app.config['SQLALCHEMY_DATABASE_URI'] = config['database_uri']
 
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['MAX_CONTENT_LENGTH'] = config['max_file_size'] * 1024 * 1034  # 128 MB
+app.config['MAX_CONTENT_LENGTH'] = config['max_file_size'] * 1024 * 1024
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
